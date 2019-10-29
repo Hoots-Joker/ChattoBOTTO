e14cd4f68a15e577b7f3d314f18f19cd045ff804
Hoots-As <igorfael.2000@gmail.com>
2019-10-29 20:14:39 -0300

    Animaçoes

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,4 +14,23 @@ $(function() {
             $('.navbar').removeClass('scroll');
         }
     });
+
+    // :: Deixar o input Ativo
+    const $input = $('.input-newsletter');
+
+    $input.on('focusout', () => {
+        if($input.val() != "") {
+            $('.input-newsletter').addClass('active');
+            $('.btn-newsletter').addClass('active');
+        } else {
+            $('.input-newsletter').removeClass('active');
+            $('.btn-newsletter').removeClass('active');
+        }
+    });
+
+    // :: AOS
+    AOS.init({
+        duration: 1000,
+        once: true
+    });
 });
